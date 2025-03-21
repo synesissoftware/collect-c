0b0730423e2c62873fa05dacdd26dcd7007e70bd
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-03-21 14:25:59 +1100

    ~ fixed missing forward declaration

--- include/collect-c/tmap.h
+++ include/collect-c/tmap.h
@@ -26,7 +26,7 @@
 
 #define COLLECT_C_TMAP_VER_MAJOR        0
 #define COLLECT_C_TMAP_VER_MINOR        0
-#define COLLECT_C_TMAP_VER_PATCH        1
+#define COLLECT_C_TMAP_VER_PATCH        2
 #define COLLECT_C_TMAP_VER_ALPHABETA    1
 
 #define COLLECT_C_TMAP_VER \
